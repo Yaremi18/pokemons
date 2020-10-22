ec1ad8b3d13e2b9bb2a1143e9a943ac1a1b5a22b
Yaremi Astorga <yare1894@gmail.com>
2020-10-22 00:09:25 -0600

    refactor: Return formated data

--- src/hooks/usePokemons.js
+++ src/hooks/usePokemons.js
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useState } from 'react'
+import replaceStrings from '../shared/replaceStrings'
 
 const usePokemons = () => {
     const [loading, setLoading] = useState(true)
@@ -7,12 +8,25 @@ const usePokemons = () => {
     const getPokemons = useCallback(async () => {
         console.log("Enter here")
         try {
-            const responseUrls = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+            const responseUrls = await fetch('https://pokeapi.co/api/v2/pokemon?limit=15')
             const _pokemonsFetch = (await responseUrls.json()).results || []
 
             const _pokemons = await Promise.all(_pokemonsFetch.map(async(pokemon) => {
                 const responseData = await fetch(pokemon.url)
-                return responseData.json()
+                const _pokemon = await responseData.json()
+
+                return {
+                    id: _pokemon.id,
+                    name: _pokemon.name,
+                    height: _pokemon.height / 10, // Convert to meters
+                    weight: _pokemon.weight / 10, // Convert to kg
+                    types: _pokemon.types.map((item) => replaceStrings(item.type.name, ['-'])),
+                    abilities: _pokemon.abilities.map((item) => replaceStrings(item.ability.name, ['-'])),
+                    moves: _pokemon.moves.map((item) => replaceStrings(item.move.name, ['-'])),
+                    forms: _pokemon.forms.map((form) => replaceStrings(form.name, ['-'])),
+                    sprites: _pokemon.sprites,
+                    location_area_encounters: _pokemon.location_area_encounters,
+                }
             }))
 
             setPokemons(_pokemons)
